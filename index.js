2ea085bf23d1eac2e5d4c13a5877b57896952e1d
David Engelen <robelene112@gmail.com>
2023-07-25 19:00:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require('node:path')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+   res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+})
